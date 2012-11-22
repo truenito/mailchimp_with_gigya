5193d1b60fda09cdd9497f529073a6abcae95dcb
Gabriel Gonzalez <cerito@EstiloPanda.local>
2012-11-22 00:41:33 -0400

    Initial Upload

---
+++ app.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+require 'sinatra'
+require 'hominid'
+
+configure do
+  # API Key Stuff
+  set :mailchimp_api_key, "f36ec720a12f3f66a04878caa5badc25-us6"
+  set :mailchimp_list_name, "Test"
+end
+
+get '/' do
+ erb :index
+end
+
+get '/gigya' do
+ erb :gigya
+end
+
+get '/gigyaView' do
+ erb :gigyaView
+end
+
+#Subcribe to MailChimp "Test" list @TrueniTo
+post '/signup' do
+  email = params[:email]
+  #Realiza la acción de abajo al menos que el email esté vacio.
+  unless email.nil? || email.strip.empty?
+    mailchimp = Hominid::API.new(settings.mailchimp_api_key)
+    list_id = mailchimp.find_list_id_by_name(settings.mailchimp_list_name)
+    raise "No se pudo sacar el ID de la lista por el nombre." unless list_id
+    mailchimp.list_subscribe(list_id, email, {}, 'html', false, true, true, false)
+  end
+  "NICE"
+end
+
+#Unsubscribe to MailChimp "Test" list @TrueniTo
+#post '/unsubscribe' do
+#  email = params[:email]
+#    mailchimp = Hominid::API.new(settings.mailchimp_api_key)
+#    list_id = mailchimp.find_list_id_by_name(settings.mailchimp_list_name)
+#    listUnsubscribe("f36ec720a12f3f66a04878caa5badc25-us6", list_id, email, false, false, false)
+#  "Unsubscribe"
+#end
